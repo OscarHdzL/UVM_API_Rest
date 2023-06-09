a241f533d55d8434edda305ecddd705c9e21eaae
Oscar Hernández <oscar.hernandez@alcegix.mx>
2023-06-09 11:22:50 -0600

    Se agrega Periodo

---
+++ Negocio/CatPeriodoNegocio.cs
@@ -0,0 +1,159 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Negocio.Respuesta;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Datos.ModelosDBSIAC.Entities;
+
+namespace Negocio
+{
+    public class CatPeriodoNegocio
+    {
+        public AppSIACDbContext ctx = new AppSIACDbContext();
+
+        public TipoAccion Respuesta { get; set; }
+
+
+        public async Task<TipoAccion> Get(int? id, int pageSize, int pageNumber)
+        {
+            try
+            {
+                List<CatPeriodo> lista = new List<CatPeriodo>();
+
+
+                if (pageSize == 0)
+                    throw new Exception("El parámetro pageSize debe ser mayor a cero");
+
+                var resultados = await ctx.CatPeriodos.FromSqlInterpolated($@"EXEC sp_Periodo_Select @PageSize = {pageSize}, @PageNumber = {pageNumber}").ToListAsync();
+                Respuesta = TipoAccion.Positiva(resultados);
+
+            }
+            catch (Exception ex)
+            {
+                Respuesta = TipoAccion.Negativa(ex.Message);
+            }
+
+            return Respuesta;
+        }
+
+
+
+        public async Task<TipoAccion> Insertar(CatPeriodo entidad)
+        {
+            try
+            {
+                var parametroId = new SqlParameter("@idRespuesta", SqlDbType.Int);
+                parametroId.Direction = ParameterDirection.Output;
+
+                var parametroExito = new SqlParameter("@exito", SqlDbType.Int);
+                parametroExito.Direction = ParameterDirection.Output;
+
+                var parametroMensaje = new SqlParameter("@mensaje", SqlDbType.NVarChar, -1);
+                parametroMensaje.Direction = ParameterDirection.Output;
+
+
+                await ctx.Database.ExecuteSqlInterpolatedAsync($@"EXEC sp_Periodo_Actualiza
+                                    @TipoActualiza = {"I"}, @Id = {0}, @Etapa = {entidad.Etapa}, @Activo = {entidad.Activo}, @FechaInicial = {entidad.FechaInicial}, @FechaFinal = {entidad.FechaFinal}, @FechaCreacion = {entidad.FechaCreacion}, 
+                                    @UsuarioCreacion = {entidad.UsuarioCreacion}, @FechaModificacion = {entidad.FechaModificacion}, 
+                                    @UsuarioModificacion = {entidad.UsuarioModificacion}, 
+                                    @idRespuesta = {parametroId} OUTPUT, @exito = {parametroExito} OUTPUT,  @mensaje = {parametroMensaje} OUTPUT");
+
+
+                Respuesta = new TipoAccion();
+                Respuesta.id = (int)parametroId.Value;
+                Respuesta.Exito = (int)parametroExito.Value == 1 ? true : false;
+                Respuesta.Mensaje = (string)parametroMensaje.Value;
+
+
+            }
+            catch (Exception ex)
+            {
+                Respuesta = TipoAccion.Negativa(ex.Message);
+            }
+
+            return Respuesta;
+
+        }
+
+
+        public async Task<TipoAccion> Actualizar(CatPeriodo entidad)
+        {
+            try
+            {
+                var parametroId = new SqlParameter("@idRespuesta", SqlDbType.Int);
+                parametroId.Direction = ParameterDirection.Output;
+
+                var parametroExito = new SqlParameter("@exito", SqlDbType.Int);
+                parametroExito.Direction = ParameterDirection.Output;
+
+                var parametroMensaje = new SqlParameter("@mensaje", SqlDbType.NVarChar, -1);
+                parametroMensaje.Direction = ParameterDirection.Output;
+
+
+                await ctx.Database.ExecuteSqlInterpolatedAsync($@"EXEC sp_Periodo_Actualiza
+                                    @TipoActualiza = {"U"}, @Id = {entidad.Id}, @Etapa = {entidad.Etapa}, @Activo = {entidad.Activo}, @FechaInicial = {entidad.FechaInicial}, @FechaFinal = {entidad.FechaFinal}, @FechaCreacion = {entidad.FechaCreacion}, 
+                                    @UsuarioCreacion = {entidad.UsuarioCreacion}, @FechaModificacion = {entidad.FechaModificacion}, 
+                                    @UsuarioModificacion = {entidad.UsuarioModificacion}, 
+                                    @idRespuesta = {parametroId} OUTPUT, @exito = {parametroExito} OUTPUT,  @mensaje = {parametroMensaje} OUTPUT");
+
+
+
+                Respuesta = new TipoAccion();
+                Respuesta.id = (int)parametroId.Value;
+                Respuesta.Exito = (int)parametroExito.Value == 1 ? true : false;
+                Respuesta.Mensaje = (string)parametroMensaje.Value;
+
+            }
+            catch (Exception ex)
+            {
+                Respuesta = TipoAccion.Negativa(ex.Message);
+            }
+
+            return Respuesta;
+
+        }
+
+        public async Task<TipoAccion> Deshabilitar(int id)
+        {
+            try
+            {
+                var parametroId = new SqlParameter("@idRespuesta", SqlDbType.Int);
+                parametroId.Direction = ParameterDirection.Output;
+
+                var parametroExito = new SqlParameter("@exito", SqlDbType.Int);
+                parametroExito.Direction = ParameterDirection.Output;
+
+                var parametroMensaje = new SqlParameter("@mensaje", SqlDbType.NVarChar, -1);
+                parametroMensaje.Direction = ParameterDirection.Output;
+
+
+                await ctx.Database.ExecuteSqlInterpolatedAsync($@"EXEC sp_Periodo_Actualiza
+                                    @TipoActualiza = {"D"}, @Id = {id},  @Etapa = {null}, @Activo = {false}, @FechaInicial = {null}, @FechaFinal = {null}, @FechaCreacion = {null}, 
+                                    @UsuarioCreacion = {null}, @FechaModificacion = {null}, 
+                                    @UsuarioModificacion = {null}, 
+                                    @idRespuesta = {parametroId} OUTPUT, @exito = {parametroExito} OUTPUT,  @mensaje = {parametroMensaje} OUTPUT");
+
+
+                Respuesta = new TipoAccion();
+                Respuesta.id = (int)parametroId.Value;
+                Respuesta.Exito = (int)parametroExito.Value == 1 ? true : false;
+                Respuesta.Mensaje = (string)parametroMensaje.Value;
+
+            }
+            catch (Exception ex)
+            {
+                Respuesta = TipoAccion.Negativa(ex.Message);
+            }
+
+            return Respuesta;
+
+        }
+
+
+    }
+}
---
+++ UVMApi/Controllers/CatPeriodoController.cs
@@ -0,0 +1,65 @@
+using Datos.Modelos;
+using Datos.ModelosDBSIAC.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using Negocio;
+using Negocio.Respuesta;
+
+namespace UVMApi.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    //[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class CatPeriodoController : ControllerBase
+    {
+
+
+        private readonly ILogger<CatPeriodoController> _logger;
+        public CatPeriodoNegocio negocio = new CatPeriodoNegocio();
+
+        public CatPeriodoController(ILogger<CatPeriodoController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        public Task<TipoAccion> GetAll(int pageNumber = 1, int pageSize = 5)
+        {
+            return negocio.Get(null, pageSize, pageNumber);
+        }
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //public Task<TipoAccion> GetById(int id)
+        //{
+        //    //PAGINA 0, NO REGISTROS 1
+        //    return negocio.Get(id, 1, 0);
+        //}
+
+        [HttpPost]
+        [Route("[action]")]
+        public Task<TipoAccion> Add(CatPeriodo entidad)
+        {
+            return negocio.Insertar(entidad);
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public Task<TipoAccion> Update(CatPeriodo entidad)
+        {
+            return negocio.Actualizar(entidad);
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        public Task<TipoAccion> Disable(int id)
+        {
+            //PAGINA 0, NO REGISTROS 1
+            return negocio.Deshabilitar(id);
+        }
+    }
+}
